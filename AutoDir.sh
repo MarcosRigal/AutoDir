1b814be121aa686a1f788fa52612105e23dc1bd1
MarcosRigal <riveragavilanmarcos@gmail.com>
2021-04-16 14:37:55 +0200

    Refactor

--- AutoDir.sh
+++ AutoDir.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-declare -i n extenstion
-echo Introduzca el número de carpetas que desea crear: 
-read n
-echo Introduzca la extenstion del archivo que se incluirá en la carpeta: 
-read 
+read -p "Introduzca el número de carpetas que desea crear: " numero
+read -p "Introduzca la extenstion del archivo que se incluirá en la carpeta: " extension
 i=0
-while [  $i -lt $n ]; do
+while [ $i -lt $numero ]; 
+do
     let i+=1
     mkdir E$i
     cd E$i
-    touch E$i.${REPLY}
+    touch E$i.$extension
+    if [ $extension == "sh" ]
+    then
+        chmod u+x E$i.sh
+    fi
     cd ..
 done
